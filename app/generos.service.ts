1c3238fbaff6ebeeb309b50dfb9042eee8124943
wwanda20 <wandawaridel@hotmail.com>
2020-08-28 11:49:46 -0300

    actualizacion

--- app/generos.service.ts
+++ app/generos.service.ts
@@ -9,16 +9,28 @@ export class GenerosService {
   //establezco los generos y id dentro de un array
   generosLibros = [
     {
-      id: 'idAventura',
-      genero: 'Aventura'
+      id: 'idAccion',
+      genero: 'Accion'
     },
     {
-      id: 'idFiccion',
-      genero: 'Ficcion'
+      id: 'idAutoayuda',
+      genero: 'Autoayuda'
+    },
+    {
+      id: 'idManual',
+      genero: 'Manual'
+    },
+    {
+      id: 'idMisterio',
+      genero: 'Misterio'
     },
     {
       id: 'idNovela',
       genero: 'Novela'
+    },
+    {
+      id: 'idReligion',
+      genero: 'Religion'
     },
     {
       id: 'idTerror',
