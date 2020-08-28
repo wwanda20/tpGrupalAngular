6ff1c3ba64268a415c0a58c277ce1993920c6efd
wwanda20 <wandawaridel@hotmail.com>
2020-08-28 12:22:11 -0300

    actualizacion comentado

--- app/libros-prestados/libros-prestados.component.ts
+++ app/libros-prestados/libros-prestados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-libros-prestados',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./libros-prestados.component.css']
 })
 export class LibrosPrestadosComponent implements OnInit {
+
+  //@Input() libros:any = [];
 
   constructor() { }
 
